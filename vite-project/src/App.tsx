57f73225256caf7460f2bab92fe3082faba55acc
AngelovanOsch1 <99641678+AngelovanOsch1@users.noreply.github.com>
2023-10-01 15:50:17 +0200

    test lint

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -35,7 +35,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Edit <code>src/App.tsx</code> and save to test
         </p>
       </div>
       <p className='read-the-docs'>
