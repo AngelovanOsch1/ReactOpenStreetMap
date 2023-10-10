514412e64d3f6cc392fcb83ef30122739adeb8f2
Angelo <Angelo.van.osch@hotmail.com>
2023-10-10 13:04:54 +0200

    wip

---
+++ vite-project/src/Card.tsx
@@ -0,0 +1,16 @@
+import './card.scss';
+
+const Card = () => {
+  return (
+    <section>
+      <div className='location-label'></div>
+      <div>
+        <div className='kilometers-label'></div>
+        <div className='time-label'></div>
+      </div>
+      <div></div>
+    </section>
+  );
+};
+
+export default Card;
